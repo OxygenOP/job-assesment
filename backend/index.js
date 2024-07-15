0ea24716f04e7a56cf5afe89b5e984580b910cfc
Hassam Sohail <hassamsohail86@gmail.com>
2024-07-15 13:50:45 +0500

    s

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const prisma = new PrismaClient();
 const app = express();
@@ -73,6 +75,66 @@ app.put("/task/:id", async (req, res) => {
 app.delete("/task/:id", async (req, res) => {
   await prisma.task.delete({ where: { id: req.params.id } });
   res.json({ success: true });
+});
+
+async function authenticateUser({ userid, token }) {}
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({ where: { email: email } });
+  const isPasswordCorrect = bcrypt.compare(password, user.password);
+  if (user && isPasswordCorrect) {
+    const jwtToken = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.secret,
+      { expiresIn: "1h" }
+    );
+
+    res.send({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: jwtToken,
+    });
+  } else if (!isPasswordCorrect) {
+    return Error("Password doesn't match");
+  } else if (!user) {
+    return Error("User with the email doesn't exist");
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const encryptedPassword = bcrypt.hash(password);
+
+  //Check if user already exists:
+  const userEmail = prisma.user.findUnique({ where: { email: email } });
+  if (userEmail) {
+    return Error("User with email already exists");
+  } else {
+    const user = await prisma.user.create({
+      data: { name, email, password: encryptedPassword },
+    });
+    const jwtToken = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.secret,
+      { expiresIn: "1h" }
+    );
+
+    res.send({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: jwtToken,
+    });
+  }
 });
 
 app.listen(port, () => {
